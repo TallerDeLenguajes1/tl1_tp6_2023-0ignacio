4674a4a5970ea7c7fcd301e3d9e413683eab5429
0ignacio <ignaciogonzaley@gmail.com>
2023-06-14 21:53:32 -0300

    ejercicio 2 terminado

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -28,3 +28,39 @@
 // }else{
 //     Console.WriteLine("No ingresaste un numero.");
 // }
+
+//------EJERCICIO 2------
+float a, b, c = 0;
+int aux;
+do
+{
+    Console.WriteLine("CALCULADORA\n1 - Suma\n2 - Resta\n3 - Multiplicacion\n4 - Division\n0 - Salir\nIngrese una opcion: ");
+    int.TryParse(Console.ReadLine(), out aux);
+    if (aux != 0)
+    {
+        Console.Write("Ingrese el valor de a: ");
+        float.TryParse(Console.ReadLine(), out a);
+
+        Console.Write("Ingrese el valor de b: ");
+        float.TryParse(Console.ReadLine(), out b);
+        switch (aux)
+        {
+            case 1:
+                c = a + b;
+                break;
+            case 2:
+                c = a - b;
+                break;
+            case 3:
+                c = a * b;
+                break;
+            case 4:
+                c = a / b;
+                break;
+            default:
+                break;
+        }
+        Console.WriteLine("\nEl resultado de la operacion entre " + a + " y " + b + " es: " + c);
+    }
+} while (aux != 0);
+Console.WriteLine("\nEL PROGRAMA FINALIZO");
