ce79d17c69fff4436d3f5a3cdae3a17f4f1238e8
0ignacio <ignaciogonzaley@gmail.com>
2023-06-14 21:47:14 -0300

    primera parte del tp terminada

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//------PUNTO 6------
 // Console.WriteLine("Hello, World!");
 // int a;
 // int b;
@@ -8,9 +9,7 @@
 // Console.WriteLine("valor de b:"+b);
 
 
-
-
-
+//------EJERCICIO 1------
 // int num = 0;
 // string aux2;
 // int resto;
@@ -29,42 +28,3 @@
 // }else{
 //     Console.WriteLine("No ingresaste un numero.");
 // }
-
-
-
-
-float a, b, c = 0;
-int aux;
-do
-{
-    Console.WriteLine("CALCULADORA\n1 - Suma\n2 - Resta\n3 - Multiplicacion\n4 - Division\n0 - Salir\nIngrese una opcion: ");
-    int.TryParse(Console.ReadLine(), out aux);
-    if (aux != 0)
-    {
-        Console.Write("Ingrese el valor de a: ");
-        float.TryParse(Console.ReadLine(), out a);
-
-        Console.Write("Ingrese el valor de b: ");
-        float.TryParse(Console.ReadLine(), out b);
-        switch (aux)
-        {
-            case 1:
-                c = a + b;
-                break;
-            case 2:
-                c = a - b;
-                break;
-            case 3:
-                c = a * b;
-                break;
-            case 4:
-                c = a / b;
-                break;
-            default:
-                break;
-        }
-        Console.WriteLine("\nEl resultado de la operacion entre " + a + " y " + b + " es: " + c);
-    }
-} while (aux != 0);
-Console.WriteLine("\nEL PROGRAMA FINALIZO");
-
